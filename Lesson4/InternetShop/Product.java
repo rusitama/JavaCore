fe365d29e7e17370a59a32184cad9618b2e2e595
rustam <rusitama@gmail.com>
2023-08-06 18:40:29 +0500

    Add class Product

---
+++ Lesson4/InternetShop/Product.java
@@ -0,0 +1,15 @@
+package org.example;
+
+class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
